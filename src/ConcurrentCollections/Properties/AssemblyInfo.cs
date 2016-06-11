ab1abcabd6be2f515dd11cafd7d55dc6ce163db3
Bar Arnon <i3arnon@gmail.com>
2016-06-12 00:44:35 +0300

    initial implementation

---
+++ src/ConcurrentCollections/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConcurrentHashSet")]
+[assembly: AssemblyDescription("A thread-safe hash-based unique collection")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Bar Arnon")]
+[assembly: AssemblyProduct("ConcurrentHashSet")]
+[assembly: AssemblyCopyright("Copyright © Bar Arnon 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("58063048-c7b0-4f4d-b27e-1ed5f9789af7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
