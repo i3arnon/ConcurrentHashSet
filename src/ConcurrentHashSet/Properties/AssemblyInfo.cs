bffd861873ac54ae0d0d43bfd51ef17c5e7e255d
Bar Arnon <i3arnon@gmail.com>
2016-07-02 20:35:26 +0300

    Move to project.json and support .NET Standard

--- src/ConcurrentHashSet/Properties/AssemblyInfo.cs
+++ src/ConcurrentHashSet/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -9,17 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Bar Arnon")]
 [assembly: AssemblyProduct("ConcurrentHashSet")]
-[assembly: AssemblyCopyright("Copyright © Bar Arnon 2016")]
+[assembly: AssemblyCopyright("Copyright 2016 Bar Arnon")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("58063048-c7b0-4f4d-b27e-1ed5f9789af7")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +22,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
